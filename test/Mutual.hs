d34ef74bdb4fe322b6ea5203bc696500dde31487
Dan Rosén <danr@student.chalmers.se>
2012-04-27 16:49:19 +0200

    Document interesting behavior with mutual functions

--- test/Mutual.hs
+++ test/Mutual.hs
@@ -8,7 +8,40 @@ data Bool = True | False
 not True      = False
 not False     = True
 
+{- This is strange... GHC optimizes this to:
+
+      odd_aa1 = \ ds_da9 ->
+           case ds_da9 of _ {
+             Succ x_a9W ->
+               case x_a9W of _ {
+                 Succ x_a9V -> odd_aa1 x_a9V;
+                 -- ^ odd now does recursion in depth two
+
+                 Zero -> False
+               };
+             Zero -> True
+           };
+
+      even_aa2 = \ ds_dac ->
+           case ds_dac of _ {
+             Succ x_a9V ->
+               case odd_aa1 x_a9V of _ {
+                 -- ^ Where does this case come from?
+
+                 True -> False;
+                 False -> True
+               };
+             Zero -> True
+           }
+
+  It's like even has been inlined in odd, and then even has been
+  redefined in terms of it. Quite fantastic, but it's not really the
+  same program :)
+
+-}
+
 even (Succ x) = not (odd x)
 even Zero     = True
+
 odd (Succ x)  = not (even x)
 odd Zero      = True
