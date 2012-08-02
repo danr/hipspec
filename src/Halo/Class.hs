b62fe93de93f4d51a155d491ff4bb624e69de5a4
Dan Rosén <danr@student.chalmers.se>
2012-08-02 15:49:51 +0100

    Wibbles to Halo.Class

--- src/Halo/Class.hs
+++ src/Halo/Class.hs
@@ -18,6 +18,7 @@ import TyCon
 
 import Halo.Names
 import Halo.Subtheory
+import Halo.Util
 
 import Data.Maybe
 
@@ -27,15 +28,17 @@ classBinds ty_cons =
         flip (foldr Lam) (classTyVars cls ++ [v']) $
             Case (Var v') w' (varType v')
                 [(DataAlt dc,ty_vars ++ xs',Var (xs' !! i))]
-    | ty_con <- ty_cons
-    , Just cls <- [tyConClass_maybe ty_con]
-    , DataTyCon [dc] _ <- [algTyConRhs ty_con]
+    | cls <- classes
+    , DataTyCon [dc] _ <- [algTyConRhs (classTyCon cls)]
     , let v:w:xs  = varNames
           [v',w'] = map (`setVarType` (snd . splitFunTys . dropForAlls $ dataConType dc)) [v,w]
           ty_vars = dataConAllTyVars dc
           xs'     = zipWith (\u m -> setVarType u (varType m)) xs (classMethods cls)
     , (i,method_id) <- zip [0..] (classMethods cls)
     ]
+  where
+    classes = nubSorted (mapMaybe tyConClass_maybe ty_cons)
 
 dictDeps :: CoreExpr -> [Content s]
-dictDeps = functions . varSetElems . exprSomeFreeVars (\v -> isId v && isJust (isClassOpId_maybe v))
+dictDeps = functions . varSetElems
+         . exprSomeFreeVars (\v -> isId v && isJust (isClassOpId_maybe v))
