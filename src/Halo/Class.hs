bb6bbdb162a0536fe5ff5368dba6f28a4c02a242
Dan Rosén <danr@student.chalmers.se>
2012-08-03 10:12:18 +0100

    Make classBinds take a list of Class instead of TyCon

--- src/Halo/Class.hs
+++ src/Halo/Class.hs
@@ -22,22 +22,20 @@ import Halo.Util
 
 import Data.Maybe
 
-classBinds :: [TyCon] -> [CoreBind]
-classBinds ty_cons =
+classBinds :: [Class] -> [CoreBind]
+classBinds classes =
     [ NonRec method_id $
         flip (foldr Lam) (classTyVars cls ++ [v']) $
             Case (Var v') w' (varType v')
                 [(DataAlt dc,ty_vars ++ xs',Var (xs' !! i))]
     | cls <- classes
-    , DataTyCon [dc] _ <- [algTyConRhs (classTyCon cls)]
-    , let v:w:xs  = varNames
+    , let DataTyCon [dc] _ = algTyConRhs (classTyCon cls)
+          v:w:xs  = varNames
           [v',w'] = map (`setVarType` (snd . splitFunTys . dropForAlls $ dataConType dc)) [v,w]
           ty_vars = dataConAllTyVars dc
           xs'     = zipWith (\u m -> setVarType u (varType m)) xs (classMethods cls)
     , (i,method_id) <- zip [0..] (classMethods cls)
     ]
-  where
-    classes = nubSorted (mapMaybe tyConClass_maybe ty_cons)
 
 dictDeps :: CoreExpr -> [Content s]
 dictDeps = functions . varSetElems
