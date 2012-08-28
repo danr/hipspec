ae8a502353b2a51087bcad5b9c1b6b319d4603aa
Dan Rosén <danr@student.chalmers.se>
2012-08-28 15:23:58 +0100

    Remove unused squishy booleans flag from Conf

--- src/Halo/Conf.hs
+++ src/Halo/Conf.hs
@@ -5,18 +5,15 @@
 -}
 module Halo.Conf where
 
-
 data HaloConf = HaloConf
     { use_min           :: Bool
     -- ^ Use min translation
-    , use_minrec  :: Bool
+    , use_minrec        :: Bool
     -- ^ Use minrec translation
     , unr_and_bad       :: Bool
     -- ^ Use UNR and BAD when translating
     , ext_eq            :: Bool
     -- ^ Make function pointer axioms depend on extensional equality
-    , disjoint_booleans :: Bool
-    -- ^ Always add true /= false even though they might not be min
     , or_discr          :: Bool
     -- ^ Use or instead of and in the assumptions of discrimination axioms
     , var_scrut_constr  :: Bool
