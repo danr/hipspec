3d22dc63dbe156ae9750123900300cb469cd104a
Dan Rosén <danr@student.chalmers.se>
2013-01-14 19:06:51 +0100

    Print explored theory in frontend

--- testsuite/frontend/app.coffee
+++ testsuite/frontend/app.coffee
@@ -14,17 +14,20 @@ hipspec_module.filter 'ppresfile', () -> (s) ->
 
 hipspec_module.factory 'config', () ->
 
-    prod:
+    [
+        short: 'prod'
         name: 'Productive Use of Failure'
         files: ['Definitions.hs', 'Properties.hs']
-    zeno:
+    ,
+        short: 'zeno'
         name: 'Zeno/IsaPlanner'
         files: ['Definitions.hs', 'Properties.hs']
-    mini:
+    ,
+        short: 'mini'
         name: 'Mini'
         files: ['Mini.hs']
-
-    examples:
+    ,
+        short: 'examples'
         name: 'Examples'
         files:
             [ 'AppendLists.hs'
@@ -37,15 +40,17 @@ hipspec_module.factory 'config', () ->
               'Rotate.hs'
             ]
 
-    'precision-recall':
+    ,
+        short: 'precision-recall'
         name: 'Precision/Recall'
         files: []
+    ]
 
 hipspec_module.factory 'request', ($http) ->
 
-    list: (testsuite) -> $http.get("#{testsuite}/json_list")
+    list: (testsuite) -> $http.get "#{testsuite.short}/json_list"
 
-    log: (testsuite, instance) -> $http.get("#{testsuite}/#{instance}")
+    log: (testsuite, instance) -> $http.get "#{testsuite.short}/#{instance}"
 
 hipspec_module.controller 'TestsuiteCtrl', ($scope, config) ->
 
@@ -54,99 +59,106 @@ hipspec_module.controller 'TestsuiteCtrl', ($scope, config) ->
     $scope.testsuites = config
 
     $scope.testsuite = undefined
-    $scope.selected = null
 
-    $scope.setTestsuite = (v,n,fs) ->
-        $scope.selected = null
-        $scope.testsuite_name = n
-        $scope.testsuite_files = fs
-        $scope.testsuite = v
+    $scope.setTestsuite = (t) -> $scope.testsuite = t
 
 hipspec_module.controller 'CompareCtrl', ($scope, request, $http, $q) ->
 
     $scope.content = ""
     $scope.viewFile = (dir,file) -> $http.get("#{dir}/#{file}").success (res) -> $scope.content = res
 
-    $scope.table = {}
-    $scope.headers = {}
-    $scope.num_solved = 0
-    $scope.solved = {}
+    empty = () ->
+        $scope.table = {}
+        $scope.headers = {}
+        $scope.num_solved = 0
+        $scope.solved = {}
 
     $scope.display_prop = (prop) -> prop.replace /^prop_/, ""
 
     $scope.num_problems = () -> _.size $scope.headers
 
+    $scope.results = {}
+
+    process_results = () ->
+        table = {}
+        headers = {}
+        num_solved = 0
+        solved = {}
+        for i, log of $scope.results
+            for [ time, message ] in log
+                [[type,obj]] = _.pairs message
+                res = {}
+                if type == "Finished"
+                    for prop in obj.unproved
+                        headers[prop] = {}
+                        res[prop] =
+                            solved: false
+                            failed: true
+                    for prop in obj.proved
+                        headers[prop] = {}
+                        res[prop] =
+                            solved: true
+                            failed: false
+                        if res[prop].solved and not solved[prop]
+                            solved[prop] = true
+                            num_solved++
+                    res.time = time
+            table[i] = res
+        $scope.headers = headers
+        $scope.table = table
+        $scope.num_solved = num_solved
+        $scope.solved = solved
+
     $scope.$watch 'testsuite', () -> if $scope.testsuite?
         request.list($scope.testsuite).success (list) ->
-            $scope.headers = {}
-            $scope.table = {}
-            $scope.num_solved = 0
-            $scope.solved = {}
+            empty()
+            $q.all(request.log($scope.testsuite, i) for i in list).then (logs) ->
+                $scope.results = _.object list, _.map logs, (res) -> res.data
+                process_results()
 
-            u = $q.all(request.log($scope.testsuite, i) for i in list)
+hipspec_module.controller 'ExplorationCtrl', ($scope) ->
 
-            u.then (logs) ->
-                headers = {}
-                table = {}
-                num_solved = 0
-                solved = {}
-                for [i,http_res] in _.zip(list,logs)
-                    log = http_res.data
-                    # console.log i, log
-                    for [ time, message ] in log
-                        [[type,obj]] = _.pairs message
-                        res = {}
-                        if type == "Finished"
-                            for prop in obj.unproved
-                                headers[prop] = {}
-                                res[prop] =
-                                    solved: false
-                                    failed: true
-                            for prop in obj.proved
-                                headers[prop] = {}
-                                res[prop] =
-                                    solved: true
-                                    failed: false
-                                if res[prop].solved and not solved[prop]
-                                    solved[prop] = true
-                                    num_solved++
-                            res.time = time
-                    table[i] = res
+    $scope.equations = []
+    $scope.explored = false
+    $scope.processed = false
+    $scope.exploration_time = 0
 
-                # console.log num_solved, solved, headers, table
+    $scope.is_explored = (instance) ->
+        if not $scope.processed
+            for [time, message] in $scope.results[instance]
+                [[type,obj]] = _.pairs message
+                if type == "ExploredTheory"
+                    $scope.equations = obj.equations
+                    $scope.explored = true
+                    $scope.exploration_time = time
+                    break
+            $scope.processed = true
+        return $scope.explored
 
-                $scope.headers = headers
-                $scope.table = table
-                $scope.num_solved = num_solved
-                $scope.solved = solved
-
-
-
-hipspec_module.controller 'InstanceCtrl', ($scope, $http, request) ->
+hipspec_module.controller 'InstanceCtrl', ($scope) ->
 
     $scope.interestingType = (type) ->
-        String _.contains ["FileProcessed","QuickSpecDone","InductiveProof","PlainProof","Finished"], type
+        String _.contains ["FileProcessed","QuickSpecDone","InductiveProof","PlainProof","Finished","ExploredTheory"], type
 
     $scope.result = []
     $scope.show = false
 
-    $scope.toggle_shown = ->
+    $scope.toggle_shown = (instance) ->
         $scope.show = !$scope.show
-        if _.isEmpty $scope.result
-            request.log($scope.testsuite,$scope.instance).success (x) ->
-                res = []
+        x = $scope.results[instance]
+        res = []
 
-                for [time, message] in x
+        for [time, message] in x
 
-                    [[type,obj]] = _.pairs message
+            [[type,obj]] = _.pairs message
 
-                    if _.isArray obj
-                        obj = {}
+            if _.isArray obj
+                obj = {}
 
-                    obj.time = time
-                    obj.type = type
+            obj.time = time
+            obj.type = type
 
-                    res.push obj
+            res.push obj
 
-                $scope.result = res
+        $scope.result = res
 
--- testsuite/frontend/js/app.js
+++ testsuite/frontend/js/app.js
@@ -28,37 +28,38 @@
   });
 
   hipspec_module.factory('config', function() {
-    return {
-      prod: {
+    return [
+      {
+        short: 'prod',
         name: 'Productive Use of Failure',
         files: ['Definitions.hs', 'Properties.hs']
-      },
-      zeno: {
+      }, {
+        short: 'zeno',
         name: 'Zeno/IsaPlanner',
         files: ['Definitions.hs', 'Properties.hs']
-      },
-      mini: {
+      }, {
+        short: 'mini',
         name: 'Mini',
         files: ['Mini.hs']
-      },
-      examples: {
+      }, {
+        short: 'examples',
         name: 'Examples',
         files: ['AppendLists.hs', 'BinLists.hs', 'Implies.hs', 'ListMonad.hs', 'Nat.hs', 'Nicomachus.hs', 'Reverse.hs', 'Rotate.hs']
-      },
-      'precision-recall': {
+      }, {
+        short: 'precision-recall',
         name: 'Precision/Recall',
         files: []
       }
-    };
+    ];
   });
 
   hipspec_module.factory('request', function($http) {
     return {
       list: function(testsuite) {
-        return $http.get("" + testsuite + "/json_list");
+        return $http.get("" + testsuite.short + "/json_list");
       },
       log: function(testsuite, instance) {
-        return $http.get("" + testsuite + "/" + instance);
+        return $http.get("" + testsuite.short + "/" + instance);
       }
     };
   });
@@ -67,41 +68,84 @@
     $scope.empty = _.isEmpty;
     $scope.testsuites = config;
     $scope.testsuite = void 0;
-    $scope.selected = null;
-    return $scope.setTestsuite = function(v, n, fs) {
-      $scope.selected = null;
-      $scope.testsuite_name = n;
-      $scope.testsuite_files = fs;
-      return $scope.testsuite = v;
+    return $scope.setTestsuite = function(t) {
+      return $scope.testsuite = t;
     };
   });
 
   hipspec_module.controller('CompareCtrl', function($scope, request, $http, $q) {
+    var empty, process_results;
     $scope.content = "";
     $scope.viewFile = function(dir, file) {
       return $http.get("" + dir + "/" + file).success(function(res) {
         return $scope.content = res;
       });
     };
-    $scope.table = {};
-    $scope.headers = {};
-    $scope.num_solved = 0;
-    $scope.solved = {};
+    empty = function() {
+      $scope.table = {};
+      $scope.headers = {};
+      $scope.num_solved = 0;
+      return $scope.solved = {};
+    };
     $scope.display_prop = function(prop) {
       return prop.replace(/^prop_/, "");
     };
     $scope.num_problems = function() {
       return _.size($scope.headers);
     };
+    $scope.results = {};
+    process_results = function() {
+      var headers, i, log, message, num_solved, obj, prop, res, solved, table, time, type, _i, _j, _k, _len, _len1, _len2, _ref, _ref1, _ref2, _ref3, _ref4;
+      table = {};
+      headers = {};
+      num_solved = 0;
+      solved = {};
+      _ref = $scope.results;
+      for (i in _ref) {
+        log = _ref[i];
+        for (_i = 0, _len = log.length; _i < _len; _i++) {
+          _ref1 = log[_i], time = _ref1[0], message = _ref1[1];
+          _ref2 = _.pairs(message)[0], type = _ref2[0], obj = _ref2[1];
+          res = {};
+          if (type === "Finished") {
+            _ref3 = obj.unproved;
+            for (_j = 0, _len1 = _ref3.length; _j < _len1; _j++) {
+              prop = _ref3[_j];
+              headers[prop] = {};
+              res[prop] = {
+                solved: false,
+                failed: true
+              };
+            }
+            _ref4 = obj.proved;
+            for (_k = 0, _len2 = _ref4.length; _k < _len2; _k++) {
+              prop = _ref4[_k];
+              headers[prop] = {};
+              res[prop] = {
+                solved: true,
+                failed: false
+              };
+              if (res[prop].solved && !solved[prop]) {
+                solved[prop] = true;
+                num_solved++;
+              }
+            }
+            res.time = time;
+          }
+        }
+        table[i] = res;
+      }
+      $scope.headers = headers;
+      $scope.table = table;
+      $scope.num_solved = num_solved;
+      return $scope.solved = solved;
+    };
     return $scope.$watch('testsuite', function() {
       if ($scope.testsuite != null) {
         return request.list($scope.testsuite).success(function(list) {
-          var i, u;
-          $scope.headers = {};
-          $scope.table = {};
-          $scope.num_solved = 0;
-          $scope.solved = {};
-          u = $q.all((function() {
+          var i;
+          empty();
+          return $q.all((function() {
             var _i, _len, _results;
             _results = [];
             for (_i = 0, _len = list.length; _i < _len; _i++) {
@@ -109,84 +153,64 @@
               _results.push(request.log($scope.testsuite, i));
             }
             return _results;
-          })());
-          return u.then(function(logs) {
-            var headers, http_res, log, message, num_solved, obj, prop, res, solved, table, time, type, _i, _j, _k, _l, _len, _len1, _len2, _len3, _ref, _ref1, _ref2, _ref3, _ref4, _ref5;
-            headers = {};
-            table = {};
-            num_solved = 0;
-            solved = {};
-            _ref = _.zip(list, logs);
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-              _ref1 = _ref[_i], i = _ref1[0], http_res = _ref1[1];
-              log = http_res.data;
-              for (_j = 0, _len1 = log.length; _j < _len1; _j++) {
-                _ref2 = log[_j], time = _ref2[0], message = _ref2[1];
-                _ref3 = _.pairs(message)[0], type = _ref3[0], obj = _ref3[1];
-                res = {};
-                if (type === "Finished") {
-                  _ref4 = obj.unproved;
-                  for (_k = 0, _len2 = _ref4.length; _k < _len2; _k++) {
-                    prop = _ref4[_k];
-                    headers[prop] = {};
-                    res[prop] = {
-                      solved: false,
-                      failed: true
-                    };
-                  }
-                  _ref5 = obj.proved;
-                  for (_l = 0, _len3 = _ref5.length; _l < _len3; _l++) {
-                    prop = _ref5[_l];
-                    headers[prop] = {};
-                    res[prop] = {
-                      solved: true,
-                      failed: false
-                    };
-                    if (res[prop].solved && !solved[prop]) {
-                      solved[prop] = true;
-                      num_solved++;
-                    }
-                  }
-                  res.time = time;
-                }
-              }
-              table[i] = res;
-            }
-            $scope.headers = headers;
-            $scope.table = table;
-            $scope.num_solved = num_solved;
-            return $scope.solved = solved;
+          })()).then(function(logs) {
+            $scope.results = _.object(list, _.map(logs, function(res) {
+              return res.data;
+            }));
+            return process_results();
           });
         });
       }
     });
   });
 
-  hipspec_module.controller('InstanceCtrl', function($scope, $http, request) {
+  hipspec_module.controller('ExplorationCtrl', function($scope) {
+    $scope.equations = [];
+    $scope.explored = false;
+    $scope.processed = false;
+    $scope.exploration_time = 0;
+    return $scope.is_explored = function(instance) {
+      var message, obj, time, type, _i, _len, _ref, _ref1, _ref2;
+      if (!$scope.processed) {
+        _ref = $scope.results[instance];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          _ref1 = _ref[_i], time = _ref1[0], message = _ref1[1];
+          _ref2 = _.pairs(message)[0], type = _ref2[0], obj = _ref2[1];
+          if (type === "ExploredTheory") {
+            $scope.equations = obj.equations;
+            $scope.explored = true;
+            $scope.exploration_time = time;
+            break;
+          }
+        }
+        $scope.processed = true;
+      }
+      return $scope.explored;
+    };
+  });
+
+  hipspec_module.controller('InstanceCtrl', function($scope) {
     $scope.interestingType = function(type) {
-      return String(_.contains(["FileProcessed", "QuickSpecDone", "InductiveProof", "PlainProof", "Finished"], type));
+      return String(_.contains(["FileProcessed", "QuickSpecDone", "InductiveProof", "PlainProof", "Finished", "ExploredTheory"], type));
     };
     $scope.result = [];
     $scope.show = false;
-    return $scope.toggle_shown = function() {
+    return $scope.toggle_shown = function(instance) {
+      var message, obj, res, time, type, x, _i, _len, _ref, _ref1;
       $scope.show = !$scope.show;
-      if (_.isEmpty($scope.result)) {
-        return request.log($scope.testsuite, $scope.instance).success(function(x) {
-          var message, obj, res, time, type, _i, _len, _ref, _ref1;
-          res = [];
-          for (_i = 0, _len = x.length; _i < _len; _i++) {
-            _ref = x[_i], time = _ref[0], message = _ref[1];
-            _ref1 = _.pairs(message)[0], type = _ref1[0], obj = _ref1[1];
-            if (_.isArray(obj)) {
-              obj = {};
-            }
-            obj.time = time;
-            obj.type = type;
-            res.push(obj);
-          }
-          return $scope.result = res;
-        });
+      x = $scope.results[instance];
+      res = [];
+      for (_i = 0, _len = x.length; _i < _len; _i++) {
+        _ref = x[_i], time = _ref[0], message = _ref[1];
+        _ref1 = _.pairs(message)[0], type = _ref1[0], obj = _ref1[1];
+        if (_.isArray(obj)) {
+          obj = {};
+        }
+        obj.time = time;
+        obj.type = type;
+        res.push(obj);
       }
+      return $scope.result = res;
     };
   });
 
