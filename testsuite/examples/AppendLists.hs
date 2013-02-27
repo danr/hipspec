714d89d5a69daab16244a75446da4c2ac31b5d65
Dan Rosén <danr@chalmers.se>
2013-02-27 11:47:55 +0100

    Update the example with AppendLists, by deforestation of Difference Lists

--- testsuite/examples/AppendLists.hs
+++ testsuite/examples/AppendLists.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveDataTypeable, TemplateHaskell, TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE DeriveDataTypeable, TemplateHaskell #-}
 module Main where
 
 import HipSpec
@@ -8,97 +8,103 @@ import Data.Typeable
 import Control.Applicative
 import Prelude (undefined,Eq,Show,Ord,return,div,sqrt,round,($),(!!),iterate,fromInteger,toInteger,Double)
 
-data ListP a = Cons a (ListP a) | Nil
-  deriving (Show,Eq,Ord,Typeable)
+(++) :: [a] -> [a] -> [a]
+(x:xs) ++ ys = x:(xs ++ ys)
+[]     ++ ys = ys
 
-(++) :: ListP a -> ListP a -> ListP a
-Cons x xs ++ ys = Cons x (xs ++ ys)
-Nil       ++ ys = ys
+reverse :: [a] -> [a]
+reverse (x:xs) = reverse xs ++ [x]
+reverse []         = []
 
-point :: a -> ListP a
-point x = Cons x Nil
-
-reverse :: ListP a -> ListP a
-reverse (Cons x xs) = reverse xs ++ point x
-reverse Nil         = Nil
-
-head :: ListP Nat -> Nat
-head (Cons x xs) = x
+head :: [Nat] -> Nat
+head (x:xs) = x
 head _ = Z
 
-tail :: ListP a -> ListP a
-tail (Cons x xs) = xs
-tail _ = Nil
+tail :: [a] -> [a]
+tail (x:xs) = xs
+tail _ = []
 
-type List = ListP Nat
-
-prop_revrev :: ListP a -> ListP a -> Prop (ListP a)
+prop_revrev :: [a] -> [a] -> Prop ([a])
 prop_revrev xs ys = reverse xs ++ reverse ys =:= reverse (ys ++ xs)
 
-prop_revinv :: ListP a -> Prop (ListP a)
+prop_revinv :: [a] -> Prop ([a])
 prop_revinv xs = reverse (reverse xs) =:= xs
 
-data SeqP a = NilS | Unit a | Append (SeqP a) (SeqP a) deriving (Eq, Ord, Typeable, Show)
+data Fun a = NilS | Unit a | Append (Fun a) (Fun a)
+  deriving (Eq, Ord, Typeable, Show)
 
-(+++) :: SeqP a -> SeqP a -> SeqP a
+(+++) :: Fun a -> Fun a -> Fun a
+{-
 NilS +++ x = x
 x +++ NilS = x
+-}
 x +++ y = x `Append` y
 
-prop_app :: SeqP a -> SeqP a -> Prop (ListP a)
+prop_app :: Fun a -> Fun a -> Prop [a]
 prop_app x y = toList x ++ toList y =:= toList (x +++ y)
 
-prop_nil :: Prop (ListP a)
-prop_nil = toList NilS =:= Nil
+prop_nil :: Prop [a]
+prop_nil = toList NilS =:= []
 
-prop_point :: a -> Prop (ListP a)
-prop_point x = toList (Unit x) =:= point x
-
-reverseS :: SeqP a -> SeqP a
+reverseS :: Fun a -> Fun a
 reverseS NilS = NilS
 reverseS (Unit x) = Unit x
 reverseS (Append x y) = Append (reverseS y) (reverseS x)
 
-prop_reverse :: SeqP a -> Prop (ListP a)
+prop_reverse :: Fun a -> Prop ([a])
 prop_reverse x = toList (reverseS x) =:= reverse (toList x)
 
-toList :: SeqP a -> ListP a
-toList NilS = Nil
-toList (Unit x) = point x
+toList :: Fun a -> [a]
+toList NilS = []
+toList (Unit x) = [x]
 toList (Append x y) = toList x ++ toList y
 
-headS :: SeqP Nat -> Nat
+eval :: Fun a -> [a] -> [a]
+eval NilS xs = xs
+eval (Unit x) xs = x:xs
+eval (Append xs ys) zs = eval xs (eval ys zs)
+
+toList' :: Fun a -> [a]
+toList' s = eval s []
+
+headS :: Fun Nat -> Nat
 headS (Unit x) = x
 headS (Append x y) = headS x
 headS _ = Z
 
-tailS :: SeqP a -> SeqP a
+tailS :: Fun a -> Fun a
 tailS (Unit x) = NilS
 tailS (Append x y) = tailS x +++ y
 tailS _ = NilS
 
-prop_head :: SeqP Nat -> Prop Nat
+prop_toLists :: Fun a -> Prop [a]
+prop_toLists s = toList s =:= toList' s
+
+prop_head :: Fun Nat -> Prop Nat
 prop_head xs = headS xs =:= head (toList xs)
 
-prop_tail :: SeqP a -> Prop (ListP a)
+prop_tail :: Fun a -> Prop [a]
 prop_tail xs = toList (tailS xs) =:= tail (toList xs)
 
-type Seq = SeqP Nat
+type Seq = Fun Nat
+
+type List = [Nat]
 
 main = hipSpec $(fileName)
     [ vars ["x","y","z"]    intType
     , vars ["s","t","u"]    seqType
     , vars ["xs","ys","zs"] listType
     , fun0 "Z"        Z
-    , fun0 "Nil"      (Nil      :: List)
-    , fun2 "Cons"     (Cons     :: Nat -> List -> List)
-    , fun1 "point"    (point    :: Nat -> List)
+    , fun0 "[]"       ([]      :: List)
+    , fun2 ":"        ((:)     :: Nat -> List -> List)
     , fun2 "++"       ((++)     :: List -> List -> List)
     , fun1 "reverse"  (reverse  :: List -> List)
     , fun2 "+++"      ((+++) :: Seq -> Seq -> Seq)
     , fun1 "reverseS" (reverseS :: Seq -> Seq)
     , fun1 "Unit"     (Unit :: Nat -> Seq)
     , fun1 "toList"   (toList :: Seq -> List)
+    , fun1 "toList'"  (toList' :: Seq -> List)
+    , fun2 "eval"     (eval :: Seq -> List -> List)
     , fun1 "head"     (head :: List -> Nat)
     , fun1 "tail"     (tail :: List -> List)
     , fun1 "headS"    (headS :: Seq -> Nat)
@@ -109,21 +115,13 @@ main = hipSpec $(fileName)
     listType = undefined :: List
     seqType  = undefined :: Seq
 
-instance Arbitrary List where
-  arbitrary = sized arbList
+instance Arbitrary a => Arbitrary (Fun a) where
+  arbitrary = sized arbFun
     where
-      arbList s = frequency
-        [ (1,return Nil)
-        , (s,Cons <$> arbitrary <*> arbList (s `div` 2))
-        ]
-
-instance Arbitrary Seq where
-  arbitrary = sized arbSeqP
-    where
-      arbSeqP s = frequency
+      arbFun s = frequency
         [ (1, return NilS)
         , (1, Unit <$> arbitrary)
-        , (s, (+++) <$> arbSeqP (s `div` 2) <*> arbSeqP (s `div` 2))
+        , (s, (+++) <$> arbFun (s `div` 2) <*> arbFun (s `div` 2))
         ]
 
 data Nat = Z | S Nat deriving (Eq, Ord, Typeable, Show)
