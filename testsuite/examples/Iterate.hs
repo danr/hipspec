9fe56293e39f0166d35f41b1c1461612a463ee6f
Dan Rosén <danr@chalmers.se>
2013-03-15 16:14:23 +0100

    Update iterate example

--- testsuite/examples/Iterate.hs
+++ testsuite/examples/Iterate.hs
@@ -5,6 +5,7 @@ import Prelude (fmap,undefined,error,Eq,Ord)
 
 import Data.Typeable
 import HipSpec.Prelude
+import Test.QuickSpec.Signature hiding (total)
 
 data List = A ::: List | Nil
   deriving (Eq,Ord,Typeable)
@@ -24,27 +25,30 @@ tail (x:::xs) = xs
 repeat x = x:::repeat x
 iterate f x = x:::iterate f (f x)
 
-cycle Nil = error "cycle: empty list"
-cycle xs  = xs' where xs' = xs ++ xs'
-
-cycle' xs = xs ++ cycle' xs
+cycle xs@(_:::_) = xs ++ cycle xs
 
 sig = signature
     [ pvars ["x","y","z"]    (undefined :: A)
     , pvars ["xs","ys","zs"] (undefined :: List)
-    , vars ["f","g"]         (undefined :: A -> A)
+    , vars1 ["f","g"]        (undefined :: A -> A)
     , blind0 "id" id
     , "Nil"     `fun0` Nil
     , ":::"     `fun2` (:::)
     , "++"      `fun2` (++)
     , "iterate" `fun2` iterate
     , "repeat"  `fun1` repeat
+    , "map"     `fun2` map
     , "cycle"   `fun1` cycle
     , "head"    `fun1` head
     , "tail"    `fun1` tail
-    , "map"     `fun2` map
 --    , withQuickCheckSize 10
     ]
+
+prop_huh :: A -> (A -> A) -> (A -> A) -> Prop List
+prop_huh x f g = total x ==> total f ==> total g ==> iterate f x =:= iterate g x
+
+prop_duh :: A -> A -> (A -> A) -> Prop List
+prop_duh x y f = total x ==> total y ==> total f ==> iterate f x =:= iterate f y
 
 instance Arbitrary List where
     arbitrary = toList `fmap` arbitrary
