8cc393d8a797038caa6b271fedc7666a41b89ac4
Dan Rosén <danr@chalmers.se>
2013-07-08 19:22:26 +0200

    Document a case which isn't handled

--- tests/Inner.hs
+++ tests/Inner.hs
@@ -5,3 +5,12 @@ map f = go
   where
     go (x:xs) = f x : go xs
     go []     = []
+
+
+-- | This one does not get inlined
+--   Eta reduction? Handle this special case?
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = go xs
+  where
+    go (x:xs) = f x : go xs
+    go []     = []
