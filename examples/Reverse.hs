e8ec71844b11069a09e7cdaa808584022e331465
Dan Rosén <danr@chalmers.se>
2014-01-23 13:54:03 +0100

    Add type signatures to reverse example

--- examples/Reverse.hs
+++ examples/Reverse.hs
@@ -4,9 +4,11 @@ import Prelude hiding ((++))
 import HipSpec
 import List ((++))
 
+rev :: [a] -> [a]
 rev (x:xs) = rev xs ++ [x]
 rev []     = []
 
+qrev :: [a] -> [a] -> [a]
 qrev []     ys = ys
 qrev (x:xs) ys = qrev xs (x:ys)
 
