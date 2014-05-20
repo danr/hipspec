377c206bde81427334049e71ad3de97efe7ee51b
Dan Rosén <danr@chalmers.se>
2014-05-20 09:32:43 +0200

    Add a snoc reverse example

---
+++ examples/SnocReverse.hs
@@ -0,0 +1,16 @@
+module SnocReverse where
+
+import Prelude hiding ((++))
+import HipSpec
+import List ((++))
+
+snoc :: a -> [a] -> [a]
+snoc x []     = [x]
+snoc x (y:ys) = y:snoc x ys
+
+rev :: [a] -> [a]
+rev (x:xs) = snoc x (rev xs)
+rev []     = []
+
+prop_equal xs = rev (rev xs) =:= []
+
