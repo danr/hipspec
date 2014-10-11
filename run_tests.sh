725f58011c3bd362e7c139ad0cf1a51660858e6f
Dan Rosén <danr@chalmers.se>
2014-10-11 11:03:03 +0200

    Try testsuite with alt-ergo, too

--- run_tests.sh
+++ run_tests.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 PR=../testsuite/precision-recall/PrecisionRecall.hs
-COMMON='--verbosity=40  --lint-poly-fol +RTS -N2 -RTS'
+COMMON='--cvc4 --alt-ergo --verbosity=40  --lint-poly-fol +RTS -N2 -RTS'
 
 cd examples
-hipspec HOF.hs         --cvc4 --auto=False                              --success=NothingUnproved  $COMMON || exit $?
-hipspec Properties.hs  --cvc4 --auto=False                              --success=NothingUnproved  $COMMON || exit $?
-hipspec $PR            --cvc4              --extra-trans=++,foldl,foldr --success=NothingUnproved  $COMMON || exit $?
-hipspec $PR            --cvc4              --extra-trans=++,map,reverse --success=NothingUnproved  $COMMON || exit $?
-hipspec Nat.hs         --cvc4              --extra-trans=*              --success=NothingUnproved  $COMMON || exit $?
-hipspec List.hs        --cvc4 --auto=False                              --success=NothingUnproved  $COMMON || exit $?
-hipspec Reverse.hs     --cvc4              --only-user-stated           --success=ProvesUserStated $COMMON || exit $?
-hipspec Join.hs        --cvc4 --auto=False                              --success=NothingUnproved  $COMMON || exit $?
-hipspec BinLists.hs    --cvc4                                           --success=NothingUnproved  $COMMON || exit $?
-hipspec Rotate.hs      --cvc4              --only-user-stated           --success=ProvesUserStated $COMMON || exit $?
+hipspec HOF.hs         --auto=False                              --success=NothingUnproved  $COMMON || exit $?
+hipspec Properties.hs  --auto=False                              --success=NothingUnproved  $COMMON || exit $?
+hipspec $PR                         --extra-trans=++,foldl,foldr --success=NothingUnproved  $COMMON || exit $?
+hipspec $PR                         --extra-trans=++,map,reverse --success=NothingUnproved  $COMMON || exit $?
+hipspec Nat.hs                      --extra-trans=*              --success=NothingUnproved  $COMMON || exit $?
+hipspec List.hs        --auto=False                              --success=NothingUnproved  $COMMON || exit $?
+hipspec Reverse.hs                  --only-user-stated           --success=ProvesUserStated $COMMON || exit $?
+hipspec Join.hs        --auto=False                              --success=NothingUnproved  $COMMON || exit $?
+hipspec BinLists.hs                                              --success=NothingUnproved  $COMMON || exit $?
+hipspec Rotate.hs                   --only-user-stated           --success=ProvesUserStated $COMMON || exit $?
 cd ..
 
 for i in $(ls tfp1/tests/*.hs); do
