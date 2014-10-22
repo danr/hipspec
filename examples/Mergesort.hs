cf4183c3805660986d4cb1d31493378694b00f96
Dan Rosén <danr@chalmers.se>
2014-10-22 15:54:39 +0200

    Fiddling with merge sort

--- examples/Mergesort.hs
+++ examples/Mergesort.hs
@@ -4,12 +4,16 @@ module Mergesort where
 import Prelude (Bool(..),undefined,Ordering(..), (&&), otherwise, not, fmap, Eq, Ord)
 import HipSpec
 import Nat
-import Test.QuickSpec.Signature
+import Test.QuickSpec.Signature hiding (ord)
 import Test.QuickSpec
 import Test.QuickSpec.Prelude
 import Test.QuickCheck hiding ((==>))
 import GHC.Types
 import Data.Typeable
+
+min Z     y     = Z
+min (S x) Z     = Z
+min (S x) (S y) = S (min x y)
 
 (<=) :: Nat -> Nat -> Bool
 Z   <= _   = True
@@ -53,6 +57,27 @@ sorted (x:y:xs)
   | otherwise = False
 sorted _      = True
 
+{-
+ordered :: [Nat] -> Bool
+ordered []     = True
+ordered (x:xs) = ord x xs
+
+ord :: Nat -> [Nat] -> Bool
+ord x [] = True
+ord x (y:ys) = if x <= y then ord y ys else False
+
+prop_merge_ord a b xs ys = ord a xs =:= True ==> ord b ys =:= True ==> ord (min a b) (merge xs ys) =:= True
+prop_merge_ordered xs ys = ordered xs =:= True ==> ordered ys =:= True ==> ordered (merge xs ys) =:= True
+-}
+
+prop_merge_sorted_lemma x y xs ys =
+    x <= y =:= True ==> sorted xs =:= True ==> sorted (y:ys) =:= True ==>
+    sorted (x:merge xs ys) =:= True ==>
+    sorted (merge xs (y:ys)) =:= True
+
+
 prop_merge_sorted xs ys = sorted xs =:= True ==> sorted ys =:= True ==> sorted (merge xs ys) =:= True
+
 prop_msort_sorted xs = sorted (msort xs) =:= True
-prop_msort_permutation xs x = count x xs =:= count x (msort xs)
+
+-- prop_msort_permutation xs x = count x xs =:= count x (msort xs)
