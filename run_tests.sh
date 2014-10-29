83f026d32e328509ab412add6177c9739a74a7db
Dan Rosén <danr@chalmers.se>
2014-10-29 09:33:56 +0100

    Try to build on travis

--- run_tests.sh
+++ run_tests.sh
@@ -6,14 +6,14 @@ PR=../testsuite/precision-recall/PrecisionRecall.hs
 COMMON="--cvc4 --alt-ergo --spass --verbosity=40  --lint-poly-fol -N$N +RTS -N4 -RTS"
 
 cd examples
-hipspec Nat.hs    --extra-trans=*                           --success=NothingUnproved  $COMMON || exit $?
-hipspec Concat.hs --extra-trans=+,++,length,map,sum,concat  --success=NothingUnproved  $COMMON || exit $?
-hipspec Rotate.hs --only-user-stated                        --success=ProvesUserStated $COMMON || exit $?
-hipspec BinLists.hs                                         --success=NothingUnproved  $COMMON || exit $?
 hipspec HOF.hs         --auto=False                         --success=NothingUnproved  $COMMON || exit $?
 hipspec Properties.hs  --auto=False                         --success=NothingUnproved  $COMMON || exit $?
+hipspec Nat.hs    --extra-trans=*                           --success=NothingUnproved  $COMMON || exit $?
+hipspec Rotate.hs --only-user-stated                        --success=ProvesUserStated $COMMON || exit $?
 hipspec List.hs        --extra-trans=++,length,map,filter,. --success=NothingUnproved  $COMMON || exit $?
 hipspec Reverse.hs                  --only-user-stated      --success=ProvesUserStated $COMMON || exit $?
+hipspec Concat.hs --extra-trans=+,++,length,map,sum,concat  --success=NothingUnproved  $COMMON || exit $?
+hipspec BinLists.hs                                         --success=NothingUnproved  $COMMON || exit $?
 # hipspec $PR                         --extra-trans=++,foldl,foldr --success=NothingUnproved  $COMMON || exit $?
 # hipspec $PR                         --extra-trans=++,map,reverse --success=NothingUnproved  $COMMON || exit $?
 cd ..
