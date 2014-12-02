d2bffdaf4605d0b20cde74d8b19f171a8c6a1dcf
Dan Rosén <danr@chalmers.se>
2014-12-02 14:06:05 +0100

    Move fpi examples to an own directory

---
+++ examples/fpi/Mergesort.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module Mergesort where
+
+import Prelude hiding ((++),uncurry)
+import HipSpec
+import List ((++))
+import Test.QuickCheck hiding ((==>))
+import GHC.Types
+import Data.Typeable
+
+count :: Integer -> [Integer] -> Int
+count n [] = 0
+count n (x:xs)
+  | n == x = 1 `plus` count n xs
+  | otherwise = count n xs
+
+msort :: [Integer] -> [Integer]
+msort []  = []
+msort [x] = [x]
+msort xs  = merge (msort (evens xs)) (msort (odds xs))
+
+evens :: [Integer] -> [Integer]
+evens []      = []
+evens (x:rs)  = x:odds rs
+
+odds :: [Integer] -> [Integer]
+odds []     = []
+odds (x:rs) = evens rs
+
+merge :: [Integer] -> [Integer] -> [Integer]
+merge (x:xs) (y:ys)
+    | x <= y = x:merge xs (y:ys)
+    | True   = y:merge (x:xs) ys
+merge xs [] = xs
+merge [] ys = ys
+
+sorted :: [Integer] -> Bool
+sorted (x:y:xs)
+  | x <= y    = sorted (y:xs)
+  | otherwise = False
+sorted _      = True
+
+{-# NOINLINE plus #-}
+plus :: Int -> Int -> Int
+plus x y = x + y
+
+-- prop_1_merge_sorted xs ys = sorted xs =:= True ==> sorted ys =:= True ==> sorted (merge xs ys) =:= True
+
+prop_2_msort_sorted xs = sorted (msort xs) =:= True
+
+-- These four are really sufficient, but the last lemma does not get triggered in Z3 (nor CVC4)
+{-
+prop_31_count_flip xs ys x = count x (xs ++ ys) =:= count x (ys ++ xs)
+
+prop_32_count_lr x xs = count x (evens xs ++ odds xs) =:= count x xs
+
+prop_33_count_merge xs ys x = count x (xs ++ ys) =:= count x (merge xs ys)
+
+prop_34_count_congl x xs ys zs = count x xs =:= count x ys ==> count x (xs ++ zs) =:= count x (ys ++ zs)
+
+prop_34_count_cong_2 x xs ys zs ws =
+    count x xs =:= count x ys ==>
+    count x zs =:= count x ws ==>
+    count x (xs ++ zs) =:= count x (ys ++ ws)
+-}
+
+{-
+prop_3_count_lr_p x xs = count x (evens xs) + count x (odds xs) =:= count x xs
+
+prop_4_count_merge xs ys x = count x xs + count x ys =:= count x (merge xs ys)
+
+prop_5_msort_tperm xs x = count x xs =:= count x (msort xs)
+-}
+
+{-
+-- these are the proved ones from
+-- hipspec Mergesort.hs -f --plain --induction --z3 --unarysize=1 --termsize=7 --extra-trans=++
+--pqs_1 xs = xs ++ [] =:= xs
+--pqs_2 xs ys zs = (xs ++ ys) ++ zs =:= xs ++ (ys ++ zs)
+pqs_3 i is js = count i (is ++ js) =:= count i (js ++ is)
+--pqs_4 i is js = count i (merge is js) =:= count i (is ++ js)
+--pqs_5 i is js ks = count i (is ++ (js ++ ks)) =:= count i (is ++ (ks ++ js))
+--pqs_6 i is js ks = count i (is ++ merge js ks) =:= count i (is ++ (js ++ ks))
+pqs_7 i is js = plus (count i is) (count i js) =:= count i (is ++ js)
+pqs_8 i is = count i (evens is ++ odds is) =:= count i is
+-- They seem to be too many to be able to prove count correctness
+-}
--- examples/fpi/MsortInts.hs
+++ examples/fpi/MsortInts.hs
@@ -1,61 +1,46 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Mergesort where
 
-import Prelude (Bool(..),undefined,Ordering(..), (&&), (||), otherwise, not, fmap, Eq, Ord, fst, snd)
+import Prelude hiding ((++),uncurry)
 import HipSpec
-import Nat
 import List ((++))
 import Test.QuickCheck hiding ((==>))
 import GHC.Types
 import Data.Typeable
 
-min Z     y     = Z
-min (S x) Z     = Z
-min (S x) (S y) = S (min x y)
-
-(<=) :: Nat -> Nat -> Bool
-Z   <= _   = True
-S _ <= Z   = False
-S x <= S y = x <= y
-
-count :: Nat -> [Nat] -> Nat
-count n [] = Z
+count :: Integer -> [Integer] -> Integer
+count n [] = 0
 count n (x:xs)
-  | n <= x && x <= n = S (count n xs)
+  | n == x = 1 + count n xs
   | otherwise = count n xs
 
-msort :: [Nat] -> [Nat]
+msort :: [Integer] -> [Integer]
 msort []  = []
 msort [x] = [x]
 msort xs  = merge (msort ys) (msort zs)
   where (ys,zs) = split xs
 
-split :: [Nat] -> ([Nat],[Nat])
+split :: [Integer] -> ([Integer],[Integer])
 split [] = ([],[])
-split [x] = ([x],[])
-split (x:y:rs) = (x:xs,y:ys)
+split (x:rs) = (x:ys,xs)
   where (xs,ys) = split rs
 
-merge :: [Nat] -> [Nat] -> [Nat]
+merge :: [Integer] -> [Integer] -> [Integer]
 merge (x:xs) (y:ys)
     | x <= y = x:merge xs (y:ys)
     | True   = y:merge (x:xs) ys
 merge xs [] = xs
 merge [] ys = ys
 
-sorted :: [Nat] -> Bool
+sorted :: [Integer] -> Bool
 sorted (x:y:xs)
   | x <= y    = sorted (y:xs)
   | otherwise = False
 sorted _      = True
 
--- app (x,y) = x ++ y
-
 uncurry f (x,y) = f x y
 
 prop_merge_sorted xs ys = sorted xs =:= True ==> sorted ys =:= True ==> sorted (merge xs ys) =:= True
-
-prop_atotal a b = a <= b || b <= a =:= True
 
 prop_msort_sorted xs = sorted (msort xs) =:= True
 
